00c9178b9eb18005c7d5b93c24cfabf6f06006d9
frenchcokes <144563579+frenchcokes@users.noreply.github.com>
2024-09-17 23:42:16 -0600

    Implemented ID

--- server.js
+++ server.js
@@ -8,6 +8,72 @@ const io = socketIo(server);
 
 const port = 3000;
 
+const mysql = require('mysql2');
+const parser = require('json2csv');
+
+require('dotenv').config();
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting: ', err.stack);
+        return;
+    }
+    console.log("Successfully connected to MySQL DB");
+
+    //Initial DB Setup
+    connection.query('CREATE DATABASE IF NOT EXISTS ' + process.env.DB_NAME, (err, results) => {
+        if (err) {
+            console.error('Error creating database: ', err.stack);
+            return;
+        }
+        console.log('Database created successfully');
+    });
+    connection.query('USE ' + process.env.DB_NAME, (err, results) => {
+        if (err) {
+            console.error('Error using database: ', err.stack);
+            return;
+        }
+    });
+    connection.query("CREATE TABLE IF NOT EXISTS books" +
+        "(id INT AUTO_INCREMENT PRIMARY KEY, " +
+        "title VARCHAR(255), " + 
+        "author VARCHAR(255), " + 
+        "genre VARCHAR(255), " + 
+        "datepublished VARCHAR(255), " +
+        "isbn VARCHAR(255))", (err, results) => {
+        if (err) {
+            console.error('Error creating table: ', err.stack);
+            return;
+        }
+        console.log('Table created successfully');
+    });
+
+    //Copy data from DB to books array
+    connection.query("SELECT * FROM books", (err, results) => {
+        if (err) {
+            console.error('Error selecting books: ', err.stack);
+            return;
+        }
+        results.forEach((book) => {
+            books.push({
+                ID: book.id,
+                title: book.title,
+                author: book.author,
+                genre: book.genre,
+                datepublished: book.datepublished,
+                isbn: book.isbn
+            });
+        });
+    });
+});
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
@@ -21,10 +87,26 @@ server.listen(port, () => {
 books = [];
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
     socket.emit('sendBooks', books);
 
+    socket.on('getCSV', () => {
+        connection.query("SELECT * FROM books", (err, results) => {
+            if(err) {
+                console.log('Error selecting books');
+                return;
+            }
+            const csv = parser.parse(results);
+            socket.emit('sendCSV', csv);
+        });
+    });
+
     socket.on('addBook', (book) => {
+        connection.query("INSERT INTO books (title, author, genre, datepublished, isbn) VALUES (?, ?, ?, ?, ?)", [book.title, book.author, book.genre, book.datepublished, book.isbn], (err, results) => {
+            if (err) {
+                console.log('Error inserting book');
+            }
+        });
+        book.ID = books.length + 1;
         books.push(book);
     });
 
@@ -43,15 +125,14 @@ io.on('connection', (socket) => {
             case 'author':
             filteredBooks = books.filter(book => book.author.includes(filters.query));
             break;
+            case 'ID':
+            filteredBooks = books.filter(book => String(book.ID).includes(filters.query));
+            break;
             default:
             filteredBooks = books;
         }
         socket.emit('sendBooks', filteredBooks);
     })
-
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-    });
 
     socket.on('getBooks', (book) => {
         socket.emit('sendBooks', books);
